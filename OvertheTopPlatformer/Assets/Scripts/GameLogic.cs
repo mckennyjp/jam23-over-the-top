96328258ba11465f25ec9680f327c5cd500dd312
drews128 <107900962+drews128@users.noreply.github.com>
2023-10-07 15:19:50 -0500

    Added win condition
    added win condition to the script in test scene

--- OvertheTopPlatformer/Assets/Scripts/GameLogic.cs
+++ OvertheTopPlatformer/Assets/Scripts/GameLogic.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class GameLogic : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TextMeshProUGUI timerText;
+    private bool timerActive = true;
+    
+    private float currentTime;
 
     void Update()
     {
-        
+        if(timerActive){
+            currentTime += Time.deltaTime;
+            timerText.text = ("Time:" + currentTime.ToString("0.00"));
+        }
+    }
+
+    public void OnWin(){
+        Debug.Log("You Won");
+        timerActive = false;
     }
 }
