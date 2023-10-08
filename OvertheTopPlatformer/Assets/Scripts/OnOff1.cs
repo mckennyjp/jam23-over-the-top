934a66000013ac839b000315df65d83d336020dd
drews128 <107900962+drews128@users.noreply.github.com>
2023-10-07 19:11:56 -0500

    Started level 3 and made on/off platforms editable speed
    yeah

--- OvertheTopPlatformer/Assets/Scripts/OnOff1.cs
+++ OvertheTopPlatformer/Assets/Scripts/OnOff1.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class OnOff1 : MonoBehaviour
 {
     [SerializeField] private Transform Origin;
+    
+    [SerializeField] private int timeToFlip;
 
     private int Ticks = 0;
     void Update()
     {
         Ticks++;
-        if (Ticks == 2000) 
+        if (Ticks == timeToFlip / 2) 
         {
             transform.position = new Vector2(Origin.position.x - 20000, Origin.position.y);
         }
-        if (Ticks == 4000) 
+        if (Ticks == timeToFlip) 
         {
             transform.position = new Vector2(Origin.position.x, Origin.position.y);
             Ticks = 0;
